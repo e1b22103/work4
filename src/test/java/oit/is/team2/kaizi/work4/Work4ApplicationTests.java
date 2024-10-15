bd60901d66f179fb260b700482189d97e6c45594
e1b22103 <e1b22103@oit.ac.jp>
2024-10-15 13:50:34 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/team2/kaizi/work4/Work4ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team2.kaizi.work4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
